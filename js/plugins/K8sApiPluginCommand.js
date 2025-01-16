d750dd8feaf77ff8bd20c18c5961cafc51cc1a05
wongcyrus <cywong@vtc.edu.hk>
2025-01-16 01:50:10 -0800

    Update game title and locale; adjust editMapId and versionId

--- js/plugins/K8sApiPluginCommand.js
+++ js/plugins/K8sApiPluginCommand.js
@@ -6,7 +6,25 @@
  */
 
 (function () {
+  'use strict';
+  let count = 0;
   // Your code goes here
+  let callApi = function () {
+    let url =
+      'https://4kig7tlso0.execute-api.us-east-1.amazonaws.com/Prod/game-task?email=cywong@vtc.edu.hk&game=game01';
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        let json = JSON.parse(xhr.response);
+        console.log(json);
+        count++;
+        console.log('count: ' + count);
+        $gameMessage.add(json.message);
+      }
+    };
+    xhr.send();
+  };
 
   var _Game_Interpreter_pluginCommand =
     Game_Interpreter.prototype.pluginCommand;
@@ -14,7 +32,7 @@
     _Game_Interpreter_pluginCommand.call(this, command, args);
     if (command === 'K8sApiPluginCommand') {
       console.log('K8sApiPluginCommand Called');
-      $gameMessage.add('K8sApiPluginCommand \n<\N[ID]> \n<\V[ID]>');
+      callApi();
     }
   };
 })();

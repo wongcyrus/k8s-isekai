b24ff86c7493dc24c4910310222bad89ca9a9ba7
wongcyrus <cywong@vtc.edu.hk>
2025-01-19 20:36:06 -0800

    Add Command parameter support for NPC name. Add text wrap for paragraph message.

--- js/plugins/NpcK8sPluginCommand.js
+++ js/plugins/NpcK8sPluginCommand.js
@@ -2,17 +2,17 @@
  * @plugindesc K8s API plugin for RPG Maker MV/MZ
  * @author Cyrus Wong
  *
- * @help This is a  plugin that send ajax request to K8s game api.
+ * @help This is a plugin that sends ajax requests to the K8s game API.
  */
 
 (function () {
   'use strict';
+  const wrapTextLength = 55;
   const urlParams = new URLSearchParams(window.location.search);
   const baseUrl = urlParams.get('baseUrl');
   const email = urlParams.get('email');
   const game = urlParams.get('game');
   let lastResponse = null;
-
   let isCalling = false;
 
   const popitup = (url) => {
@@ -20,42 +20,70 @@
     window.focus();
     return false;
   };
-  const callApi = (gameMessage) => {
+
+  // Display the text response within the window limits
+  const wrapText = (text) => {
+    const words = text.split(' ');
+    let wrappedText = '';
+    let currentLine = '';
+
+    for (const word of words) {
+      const potentialLine = currentLine + (currentLine ? ' ' : '') + word;
+      if (potentialLine.length <= wrapTextLength) {
+        currentLine = potentialLine;
+      } else {
+        wrappedText += (wrappedText ? '\n' : '') + currentLine;
+        currentLine = word;
+      }
+    }
+
+    if (currentLine) {
+      wrappedText += (wrappedText ? '\n' : '') + currentLine;
+    }
+
+    return wrappedText;
+  };
+
+  const callApi = (gameMessage, npcName) => {
     if (isCalling) return;
     isCalling = true;
 
     let url = `${baseUrl}/game-task?email=${email}&game=${game}`;
     if (lastResponse?.next_game_phrase) {
-      url = `${baseUrl}/grader?email=${email}&game=${game}&phrase=${lastResponse.next_game_phrase}`;
+      url = `${baseUrl}/grader?email=${email}&game=${game}&phrase=${lastResponse.next_game_phrase}&npcName=${npcName}`;
     }
-    let xhr = new XMLHttpRequest();
+
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         isCalling = false;
         if (xhr.status === 200) {
-          let json = JSON.parse(xhr.response);
+          const json = JSON.parse(xhr.response);
           console.log(json);
           if (json.status !== 'OK') {
             popitup(json.report_url);
           }
           if (json.message) {
-            gameMessage.add(json.message);
+            gameMessage.add(wrapText(json.message));
           }
           lastResponse = json;
+        } else {
+          gameMessage.add('Sorry I cannot connect to the server!');
         }
       }
     };
     xhr.send();
   };
 
-  var _Game_Interpreter_pluginCommand =
+  const _Game_Interpreter_pluginCommand =
     Game_Interpreter.prototype.pluginCommand;
   Game_Interpreter.prototype.pluginCommand = function (command, args) {
     _Game_Interpreter_pluginCommand.call(this, command, args);
     if (command === 'NpcK8sPluginCommand') {
-      console.log('NpcK8sPluginCommand Called');
-      callApi($gameMessage);
+      const npcName = args[0];
+      console.log('NpcK8sPluginCommand Called by ' + npcName);
+      callApi($gameMessage, npcName);
     }
   };
 })();
